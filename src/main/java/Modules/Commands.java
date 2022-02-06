668baf840da2b4993b597619d96fecda441bf490
Leonid <scorpion23997@gmail.com>
2022-02-06 17:38:04 +0200

    add basic command

--- src/main/java/Modules/Commands.java
+++ src/main/java/Modules/Commands.java
@@ -1,6 +1,8 @@
 package Modules;
 
 import Config.BotConfig;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -12,8 +14,24 @@ public class Commands extends ListenerAdapter
 
         if (args[0].equalsIgnoreCase(BotConfig.prefixCommand + "info"))
         {
+            //create window in answer
+            EmbedBuilder info = new EmbedBuilder();
+            info.setTitle(":information_source:  Information");
+            info.setDescription("This is info message");
+            info.addField("Creator", "test", false);
+            info.setColor(0xf45642);
+
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage("This is info message").queue();
+            //event.getChannel().sendMessage("This is info message").queue();
+            event.getChannel().sendMessageEmbeds(info.build()).queue();
+
+            info.clear();
+        }
+
+        if (args[0].equalsIgnoreCase(BotConfig.prefixCommand + "test"))
+        {
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage("This is test message").queue();
         }
     }
 }
